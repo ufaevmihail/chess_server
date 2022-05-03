46bc7798b50c5eeb417a4040f51a91ded9626695
ufaevmihail <michael.ufaev@yandex.ru>
2022-05-03 23:49:01 +0300

    ff

--- lobbychessserver/asgi.py
+++ lobbychessserver/asgi.py
@@ -8,13 +8,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter
 import lobbyserver.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lobbychessserver.settings')
-
+django.setup()
 #application = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
