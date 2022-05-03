ebeae7ed031288bcee5fb935b0143def820b607b
ufaevmihail <michael.ufaev@yandex.ru>
2022-05-03 21:07:27 +0300

    ff

--- lobbychessserver/lobbyserver/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/livegame/(?P<game_id>\d+)$', consumers.GameConsumer.as_asgi()),
-]
-#(?P<game_id>\d+)
